7e39c755cb79bc29346b44976aa6e06da026ecae
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-11-21 05:10:17 +0100

    test

--- pokedex/assets/js/button-main.js
+++ pokedex/assets/js/button-main.js
@@ -1,31 +1,22 @@
 // button function, remove class .visible from all classes and add class .visible to the div with ID that is given
-function pageLogin(){
-    document.querySelectorAll("*").forEach(s => {
-    s.classList.remove("visible")
-    })
-    document.getElementById("signInDiv").classList.add("visible")}
-function pageSignup(){
-    document.querySelectorAll("*").forEach(s => {
-    s.classList.remove("visible")
-    })
-    document.getElementById("loginDiv").classList.add("visible")}
-function pageHome(){
-    document.querySelectorAll("*").forEach(s => {
-    s.classList.remove("visible")
-    })
-    document.getElementById("pokeHome").classList.add("visible")}
-function pagePokedex(){
-    document.querySelectorAll("*").forEach(s => {
-    s.classList.remove("visible")
-    })
-    document.getElementById("pokeDex").classList.add("visible")}
-function pageVideogames(){
-    document.querySelectorAll("*").forEach(s => {
-    s.classList.remove("visible")
-    })
-    document.getElementById("pokeVideogames").classList.add("visible")}
-function pageMarketplace(){
-    document.querySelectorAll("*").forEach(s => {
-    s.classList.remove("visible")
-    })
-    document.getElementById("pokeMarketplace").classList.add("visible")}
+function btnClicked(clicked){
+        visible = "poke" + clicked
+            document.querySelectorAll("*").forEach(s => {
+            s.classList.remove("visible")
+        }
+    )
+    document.getElementById(visible).classList.add("visible")
+}
+
+function btnClickedSearch(clicked){
+    visible = clicked
+        document.querySelectorAll(".poke-card").forEach(s => {
+        s.classList.remove("visible")
+    }
+)
+        document.querySelectorAll("."+visible).forEach(s => {
+        s.classList.add("visible")
+    }
+)
+
+}
