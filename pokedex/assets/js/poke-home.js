3b1377b1dcc3309c0bfcc835232da769bba9261a
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-11-21 05:32:42 +0100

    no idea

--- pokedex/assets/js/poke-home.js
+++ pokedex/assets/js/poke-home.js
@@ -248,3 +248,10 @@ pokedex();
        }
       return "";
     }
+
+    function createCookie(cookieName,cookieValue,daysToExpire)
+    {
+      var date = new Date();
+      date.setTime(date.getTime()+(daysToExpire*24*60*60*1000));
+      document.cookie = cookieName + "=" + cookieValue + "; expires=" + date.toGMTString();
+    }
