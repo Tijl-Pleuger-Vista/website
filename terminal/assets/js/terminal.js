f10c9a5bde55e984fe72af99641461b0101c0464
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-01-03 22:19:11 +0100

    Fix

--- terminal/assets/js/terminal.js
+++ terminal/assets/js/terminal.js
@@ -1,27 +1,27 @@
-fetch(`https://api.my-ip.io/v2/ip.json	`)
-.then(userData => userData.json())
-.then(userData => {
-data.innerHTML +=
-`
-<p>Response. . . . . . . . . . . . . : ${userData.success}</p>
-<p>Address type. . . . . . . . . . . : ${userData.type}</p>
-<p>IPv4 Address. . . . . . . . . . . : ${userData.ip}</p>
-<br>
-<p>User Location</p>
-<p>User time zone. . . . . . . . . . : ${userData.timeZone}</p>
-<p>User region . . . . . . . . . . . : ${userData.region}</p>
-<p>User city . . . . . . . . . . . . : ${userData.city}</p>
-<p>Country code. . . . . . . . . . . : ${userData.country.code}</p>
-<p>Country name. . . . . . . . . . . : ${userData.country.name}</p>
-<br>
-<p>User location . . . . . . . . . . : ${userData.location.lat} - ${userData.location.lon}</p>
-<br>
-<p>User autonomous system number (ASN)</p>
-<p>Name. . . . . . . . . . . . . . . : ${userData.asn.name}</p>
-<p>Number. . . . . . . . . . . . . . : ${userData.asn.number}</p>
-<p>Network . . . . . . . . . . . . . : ${userData.asn.network}</p>
-`
-})
+// fetch(`https://api.my-ip.io/v2/ip.json	`)
+// .then(userData => userData.json())
+// .then(userData => {
+// data.innerHTML +=
+// `
+// <p>Response. . . . . . . . . . . . . : ${userData.success}</p>
+// <p>Address type. . . . . . . . . . . : ${userData.type}</p>
+// <p>IPv4 Address. . . . . . . . . . . : ${userData.ip}</p>
+// <br>
+// <p>User Location</p>
+// <p>User time zone. . . . . . . . . . : ${userData.timeZone}</p>
+// <p>User region . . . . . . . . . . . : ${userData.region}</p>
+// <p>User city . . . . . . . . . . . . : ${userData.city}</p>
+// <p>Country code. . . . . . . . . . . : ${userData.country.code}</p>
+// <p>Country name. . . . . . . . . . . : ${userData.country.name}</p>
+// <br>
+// <p>User location . . . . . . . . . . : ${userData.location.lat} - ${userData.location.lon}</p>
+// <br>
+// <p>User autonomous system number (ASN)</p>
+// <p>Name. . . . . . . . . . . . . . . : ${userData.asn.name}</p>
+// <p>Number. . . . . . . . . . . . . . : ${userData.asn.number}</p>
+// <p>Network . . . . . . . . . . . . . : ${userData.asn.network}</p>
+// `
+// })
 
 window.addEventListener("keydown", function(event) {
     letter = event.key;
@@ -53,32 +53,32 @@ window.addEventListener("keydown", function(event) {
     if(letter == "y") {log.innerHTML += letter}
     if(letter == "z") {log.innerHTML += letter}
 
-    if(letter == "A") {log.innerHTML += letter}
-    if(letter == "B") {log.innerHTML += letter}
-    if(letter == "C") {log.innerHTML += letter}
-    if(letter == "D") {log.innerHTML += letter}
-    if(letter == "E") {log.innerHTML += letter}
-    if(letter == "F") {log.innerHTML += letter}
-    if(letter == "G") {log.innerHTML += letter}
-    if(letter == "H") {log.innerHTML += letter}
-    if(letter == "I") {log.innerHTML += letter}
-    if(letter == "J") {log.innerHTML += letter}
-    if(letter == "K") {log.innerHTML += letter}
-    if(letter == "L") {log.innerHTML += letter}
-    if(letter == "M") {log.innerHTML += letter}
-    if(letter == "N") {log.innerHTML += letter}
-    if(letter == "O") {log.innerHTML += letter}
-    if(letter == "P") {log.innerHTML += letter}
-    if(letter == "Q") {log.innerHTML += letter}
-    if(letter == "R") {log.innerHTML += letter}
-    if(letter == "S") {log.innerHTML += letter}
-    if(letter == "T") {log.innerHTML += letter}
-    if(letter == "U") {log.innerHTML += letter}
-    if(letter == "V") {log.innerHTML += letter}
-    if(letter == "W") {log.innerHTML += letter}
-    if(letter == "X") {log.innerHTML += letter}
-    if(letter == "Y") {log.innerHTML += letter}
-    if(letter == "Z") {log.innerHTML += letter}
+    // if(letter == "A") {log.innerHTML += letter}
+    // if(letter == "B") {log.innerHTML += letter}
+    // if(letter == "C") {log.innerHTML += letter}
+    // if(letter == "D") {log.innerHTML += letter}
+    // if(letter == "E") {log.innerHTML += letter}
+    // if(letter == "F") {log.innerHTML += letter}
+    // if(letter == "G") {log.innerHTML += letter}
+    // if(letter == "H") {log.innerHTML += letter}
+    // if(letter == "I") {log.innerHTML += letter}
+    // if(letter == "J") {log.innerHTML += letter}
+    // if(letter == "K") {log.innerHTML += letter}
+    // if(letter == "L") {log.innerHTML += letter}
+    // if(letter == "M") {log.innerHTML += letter}
+    // if(letter == "N") {log.innerHTML += letter}
+    // if(letter == "O") {log.innerHTML += letter}
+    // if(letter == "P") {log.innerHTML += letter}
+    // if(letter == "Q") {log.innerHTML += letter}
+    // if(letter == "R") {log.innerHTML += letter}
+    // if(letter == "S") {log.innerHTML += letter}
+    // if(letter == "T") {log.innerHTML += letter}
+    // if(letter == "U") {log.innerHTML += letter}
+    // if(letter == "V") {log.innerHTML += letter}
+    // if(letter == "W") {log.innerHTML += letter}
+    // if(letter == "X") {log.innerHTML += letter}
+    // if(letter == "Y") {log.innerHTML += letter}
+    // if(letter == "Z") {log.innerHTML += letter}
     
     if(letter == "Backspace") {
         var backspace=document.getElementById("log").innerText;
@@ -96,7 +96,7 @@ window.addEventListener("keydown", function(event) {
         terminal.innerHTML +=
         `
         <div class="row">
-        <p style="margin-right: 8px;">PS C:\\Users/admin></p><p class="code" id="log"></p><p class="underscore">_</p>
+        <p style="margin-right: 8px;">PS C://Users/admin></p><p class="code" id="log"></p><p class="underscore">_</p>
         </div>
         `
     }
