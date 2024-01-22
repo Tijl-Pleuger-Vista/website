6b086c9ccae42961d582b43e93158393a6179561
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-01-22 11:30:33 +0100

    Update

---
+++ app/assets/js/init.js
@@ -0,0 +1,7 @@
+const viewportHeight = window.innerHeight;
+const viewportW = window.innerWidth;
+if (viewportHeight > viewportW) {
+    window.open("https://tijl-pleuger-vista.github.io/website.github.io/terminal/warning")
+}   else    {
+    window.open("https://tijl-pleuger-vista.github.io/website.github.io/terminal/terminal")
+}
