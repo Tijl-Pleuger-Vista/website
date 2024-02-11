70fe5ce680b8a0cfecc2e4be5cbf2f7a0409c8f8
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-02-11 06:43:59 +0100

    Test

--- public/leet-handbook/assets/js/level.js
+++ public/leet-handbook/assets/js/level.js
@@ -2,7 +2,7 @@ let experienceNumber = document.getElementById("experience");
 let levelNumber = document.getElementById("level");
 
 let experienceCounter = 0;
-let experience = 80
+let experience = 50
 let level = 10
 levelNumber.innerHTML = "lvl " + level
 setInterval(() => {
