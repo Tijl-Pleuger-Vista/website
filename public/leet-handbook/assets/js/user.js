8fd3bc5d74625103e66b81c4b82c352f936d7d3a
IHeadBodyScript <IHeadBodyScript@gmail.com>
2024-03-05 01:00:20 +0100

    Firebase login

--- public/leet-handbook/assets/js/user.js
+++ public/leet-handbook/assets/js/user.js
@@ -1,39 +1,18 @@
 
+
 function divRegister(){
-  console.log("test1")
-  mewreplace.innerHTM = 
-  `
-  <div class="center column">
-      <form id="mainForm">
-          <div class="center column mb-20">
-              <input type="text" id="username" placeholder="Username">
-              <input type="email" id="email" placeholder="Email">
-              <input type="password" id="password" placeholder="New Password">    
-              <button class="mt-20" type="submit">Register</button>
-              <div class="mt-20 color">OR</div>
-              <button class="mt-20 color" onclick="divLogin()">Login instead</button>
-          </div>
-      </form>
-  </div>
-  `
+    document.getElementById('form-login').classList.add('not-visible');
+    document.getElementById('form-login').classList.remove('visible');
+
+    document.getElementById('form-register').classList.add('visible');
+    document.getElementById('form-register').classList.remove('not-visible');
 }
+
+
 function divLogin(){
-  console.log("test22")
-  mewreplace.innerHTM = 
-  `
-  <div class="center column">
-      <form id="mainForm">
-          <div class="center column mb-20">
-              <input type="email" id="email" placeholder="Email">
-              <input type="password" id="password" placeholder="New Password">    
-              <button class="mt-20" type="submit">Login</button>
-              <div class="mt-20 color">OR</div>
-              <button class="mt-20 color" onclick="divRegister()">Create an account instead</button>
-          </div>
-      </form>
-  </div>
-  <div class="mt-75">
-      <button class="color" onclick="btnClicked()">Forgot Password?</button>
-  </div>
-  `
+    document.getElementById('form-register').classList.add('not-visible');
+    document.getElementById('form-register').classList.remove('visible');
+
+    document.getElementById('form-login').classList.add('visible');
+    document.getElementById('form-login').classList.remove('not-visible');
 }
