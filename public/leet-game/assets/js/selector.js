91e995ed9ed8e87d1659a3a7ca64529013438543
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-04-06 02:42:09 +0200

    Update

--- public/leet-game/assets/js/selector.js
+++ public/leet-game/assets/js/selector.js
@@ -3,12 +3,12 @@ localStorage.removeItem("json");
 function caseCheck(check){
     switch (check) {
         case 0:
-            var fetchJson = "https://raw.githubusercontent.com/Tijl-Pleuger-Vista/website.github.io/main/public/leet-game/assets/json/pvo-en.json"
+            var fetchJson = "https://raw.githubusercontent.com/Tijl-Pleuger-Vista/website.github.io/main/public/leet-game/assets/json/en.json"
             // var fetchJson = "./assets/json/pvo-1.json"
             wait(fetchJson)
         break;
         case 1:
-            var fetchJson = "https://raw.githubusercontent.com/Tijl-Pleuger-Vista/website.github.io/main/public/leet-game/assets/json/dungeon.json"
+            var fetchJson = "https://raw.githubusercontent.com/Tijl-Pleuger-Vista/website.github.io/main/public/leet-game/assets/json/nl.json"
             wait(fetchJson)
         break;
         default:
@@ -21,6 +21,5 @@ async function wait(fetchJson) {
     var checkJson = await checkJson.json();
     localStorage.setItem("json", JSON.stringify(checkJson));
     console.log(checkJson)
-    // window.location.href = "https://tijl-pleuger-vista.github.io/website.github.io/public/leet-game/game.html";
     window.location.href = "game.html";
 }
